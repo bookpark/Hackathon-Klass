fcfee52a6f09bec3211ae29ae7c82a066802a9ca
Booki Park <bkbkgg@gmail.com>
2017-11-10 17:39:50 +0900

    templates 폴더 추가

--- klass/member/models.py
+++ klass/member/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
 class User(AbstractUser):
     email = models.EmailField(null=False, blank=False)
     is_active = models.BooleanField(
         default=False,
     )
-
