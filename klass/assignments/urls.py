3938e85a28baa5145275a82504f5256bed94748e
darkblank <darkblank1990@gmail.com>
2017-11-11 03:15:55 +0900

    submit_assignment_detail 구현, submit_assignment_list에서 하이퍼링크로 연결

--- klass/assignments/urls.py
+++ klass/assignments/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import assignment_list, assignment_detail, submit_assignment_list
+from .views import assignment_list, assignment_detail, submit_assignment_list, submit_assignment_detail
 
 urlpatterns = [
     url(r'^asm_list/$', assignment_list, name='assignment_list'),
     url(r'^asm_detail/(?P<pk>\d+)/$', assignment_detail, name='assignment_detail'),
     url(r'^sub_asm_list/(?P<pk>\d+)/$', submit_assignment_list, name='submit_assignment_list'),
+    url(r'^sub_asm_detail/(?P<pk>\d+)/$', submit_assignment_detail, name='submit_assignment_detail'),
 ]
