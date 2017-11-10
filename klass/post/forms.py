b566537090278156061ab5bb25af675ffb00f1d4
isaccchoi <isaccchoi@naver.com>
2017-11-11 01:20:11 +0900

    CommentForm 생성, question list 나타나지 않는 것 수정

--- klass/post/forms.py
+++ klass/post/forms.py
@@ -1,8 +1,17 @@
-# from django import forms
-#
-# from post.models import Post
-#
-#
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = (
+            'context',
+        )
+
+
+
 # class PostForm(forms.ModelForm):
 #     class Meta:
 #         model = Post
